fcec70caeb9d65eee1b31525d8084850f262a94b
jasqxl <jas_qxl@hotmail.com>
2018-09-18 17:19:01 +0800

    TWU Commit 2
    Added print 8 asterisks code

--- Introductory Exercise(s)/src/com/company/Exercise.java
+++ Introductory Exercise(s)/src/com/company/Exercise.java
@@ -5,7 +5,7 @@ import java.lang.*;
 public class Exercise {
     public void printAsterisk(int numberOfAsteriskToPrint) {
         for (int i = 0; i < numberOfAsteriskToPrint; i++) {
-            System.out.println("*");
+            System.out.print("*");
         }
     }
 }
--- Introductory Exercise(s)/src/com/company/Main.java
+++ Introductory Exercise(s)/src/com/company/Main.java
@@ -5,9 +5,15 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        Exercise example_1 = new Exercise();
-        example_1.printAsterisk(1);
+        Exercise practice = new Exercise();
 
+        System.out.println("\nPrint one asterisk '*':");
+        int exercise1 = 1;
+        practice.printAsterisk(exercise1);
+
+        System.out.println("\nPrint eight asterisks '********':");
+        int exercise2 = 8;
+        practice.printAsterisk(exercise2);
 
     }
 }
